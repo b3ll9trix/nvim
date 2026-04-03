07b57010e3f00c0e791f58c4bffacb79ea2ab216
Reshma Suresh <reshmasuresh3005@gmail.com>
2026-04-03 12:33:58 +0200

    upgrade: updated to a new version

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -26,12 +26,47 @@ autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
     command = "checktime",
 })
 
+-- Autosave on focus lost, buffer leave, or leaving insert mode
+autocmd({ "FocusLost", "BufLeave", "InsertLeave" }, {
+    desc = "Autosave modified buffers",
+    group = vim.api.nvim_create_augroup("autosave", { clear = true }),
+    callback = function(event)
+        local buf = event.buf
+        if vim.bo[buf].modified and vim.bo[buf].buftype == "" and vim.fn.expand("%") ~= "" then
+            vim.api.nvim_buf_call(buf, function()
+                vim.cmd("silent! write")
+            end)
+        end
+    end,
+})
+
 -- Show diagnostics in a float on hover
 autocmd("CursorHold", {
     desc = "Show diagnostics float on hover",
     group = vim.api.nvim_create_augroup("diagnostic-float", { clear = true }),
     callback = function()
         vim.diagnostic.open_float(nil, { focusable = false })
+    end,
+})
+
+
+-- Highlight word under cursor when idle
+autocmd({ "CursorHold", "CursorHoldI" }, {
+    desc = "Highlight word under cursor",
+    group = vim.api.nvim_create_augroup("highlight-word", { clear = true }),
+    callback = function()
+        local current_word = vim.fn.expand("<cword>")
+        if current_word ~= "" then
+            pcall(vim.fn.matchadd, "Search", "\\V\\<" .. vim.fn.escape(current_word, "\\") .. "\\>", -1, 100)
+        end
+    end,
+})
+
+autocmd({ "CursorMoved", "CursorMovedI", "InsertEnter" }, {
+    desc = "Clear highlight word under cursor",
+    group = vim.api.nvim_create_augroup("clear-highlight-word", { clear = true }),
+    callback = function()
+        pcall(vim.fn.matchdelete, 100)
     end,
 })
 
